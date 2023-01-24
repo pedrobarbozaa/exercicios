0649734cc1ccd923b9a543c712216667508aa6bd
pedrobarbozaa <pedrodeandrade2@gmail.com>
2023-01-24 16:03:06 -0300

    Mais exercícios

---
+++ fundamentos/secao-3-intro-javascript/dia1-primeiros-passos/exercicio-par-entre-nos.js
@@ -0,0 +1,14 @@
+// Utilize if/else para escrever um código que defina três números em constantes e retorne true se pelo menos uma das três for par. Caso contrário, ele deve retornar false.
+// Bonus: use somente um if.
+
+
+let a = 2;
+let b = 3;
+let c = 9;
+let even = false;
+
+if (a % 2 == 0 || b  % 2 == 0 || c % 2 == 0) {
+    even = true
+}
+
+console.log(even);
