56900e297d2971a8e813627f998c6321b47226eb
pedrobarbozaa <pedrodeandrade2@gmail.com>
2023-01-24 15:40:06 -0300

    Exercício 1

---
+++ fundamentos/secao-3-intro-javascript/dia1-primeiros-passos/exercicio-operadores.js
@@ -0,0 +1,15 @@
+// Elabore alguns códigos e imprima o resultado no console usando o console.log, um para cada operação aritmética básica. Seu código deve ter duas constantes, a e b, definidas no começo com os valores que serão operados. Escreva códigos para:
+// Adição (a + b)
+// Subtração (a - b)
+// Multiplicação (a * b)
+// Divisão (a / b)
+// Módulo (a % b)
+
+const a = 4
+const b = 5
+
+console.log(a + b)
+console.log(a - b)
+console.log(a * b)
+console.log(a / b)
+console.log(a % b)
